f5378e488b2594aca4b83afc4abb28014ad48cce
rhinzz <prama.sudjana@gmail.com>
2025-01-19 11:41:36 +0700

    Add all_data.csv and dashboard.py

---
+++ dashboard/dashboard.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+sns.set_theme(style='dark')
+
+st.header('Bike Sharing Dashboard')
+st.subheader('Total Users')
+
+with st.sidebar:
+    pass
