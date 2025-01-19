a4a1e06367b7f747fb8f462d93e6e327ea6244c4
rhinzz <prama.sudjana@gmail.com>
2025-01-19 11:56:10 +0700

    Add create_bike_users_daily_df function

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,6 +4,13 @@ import seaborn as sns
 import streamlit as st
 sns.set_theme(style='dark')
 
+def create_bike_users_daily_df(df):
+    bike_users_daily_df = all_df.groupby(by=['dteday']).agg({
+        "casual_day": "sum",
+        "registered_day": "sum",
+    }).reset_index()
+    return bike_users_daily_df
+
 st.header('Bike Sharing Dashboard')
 st.subheader('Total Users')
 
