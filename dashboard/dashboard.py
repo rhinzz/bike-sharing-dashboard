9941acfe943a07b3ddcf49662b950f33f7de6038
rhinzz <prama.sudjana@gmail.com>
2025-01-19 21:43:29 +0700

    Add create_bike_users_weekly_df function

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -11,6 +11,13 @@ def create_bike_users_daily_df(df):
     }).reset_index()
     return bike_users_daily_df
 
+def create_bike_users_weekly_df(df):
+    weekly_users_daily_df = df.groupby(by=['weekday']).agg({
+        "casual_day": "sum",
+        "registered_day": "sum",
+    }).reset_index()
+    return weekly_users_daily_df
+
 st.header('Bike Sharing Dashboard :bicyclist:')
 st.subheader('Total Users')
 
